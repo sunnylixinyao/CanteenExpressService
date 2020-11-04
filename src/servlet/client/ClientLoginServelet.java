c670588ac8996774feb068eb5e506fb3b8634de5
smarteasyx <71005940+smarteasyx@users.noreply.github.com>
2020-11-04 21:17:38 +0800

    Initial commit

---
+++ src/servlet/client/ClientLoginServelet.java
@@ -0,0 +1,25 @@
+package servlet.client;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author lixinyao
+ * @create 2020-11-2020/11/4-20:16
+ **/
+@WebServlet("/ClientLoginServlet")
+public class ClientLoginServelet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
